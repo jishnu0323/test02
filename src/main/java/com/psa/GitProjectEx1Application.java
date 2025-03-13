1976c67231ab65442e66a449a2185313248a9c41
jishnu0323 <jishnu230396@gmail.com>
2025-03-13 12:44:57 +0530

    Update GitProjectEx1Application.java

--- src/main/java/com/psa/GitProjectEx1Application.java
+++ src/main/java/com/psa/GitProjectEx1Application.java
@@ -8,6 +8,7 @@ public class GitProjectEx1Application {
 
 	public static void main(String[] args) {
 		int x=10;
+		float c  =1.6f;
 		SpringApplication.run(GitProjectEx1Application.class, args);
 	}
 
